e248704f6a1beaee024453abaec02f8f7277f8f0
Ahmed Shehab <rs@MacBook-Pro.local>
2023-05-05 14:56:07 +1200

    first debug version

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+
+        InitializeComponent();
+
+		MainPage = new AppShell();
+
+	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 250;
+        const int newHeight = 200;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CalculatorApp;
+
+public partial class MainPage : ContentPage
+{
+	public int count = 0;
+
+
+    public MainPage()
+	{
+		InitializeComponent();
+        BindingContext = this;
+
+    }
+
+
+    // handle event
+    void ButtonClicked(System.Object sender, System.EventArgs e)
+    {
+        Button button = (Button)sender;
+        string buttonText = button.Text;
+        HandleMathOperation(buttonText);
+    }
+
+    void ClearAll(System.Object sender, System.EventArgs e)
+    {
+        sum.Text = "";
+        fnum.Text = "";
+        snum.Text = "";
+    }
+
+    // calculator brain
+    void HandleMathOperation(string btn)
+    {
+        //double FirstInput = Convert.ToDouble(fnum.Text);
+        //double SecondInput = Convert.ToDouble(snum.Text);
+        double temp;
+        bool hasNum1 = Double.TryParse(fnum.Text.ToString(), out double FirstInput);
+        bool hasNum2 = Double.TryParse(snum.Text.ToString(), out double SecondInput);
+
+        if(hasNum1&&hasNum2)
+            switch (btn)
+            {
+                case "-":
+                    // do
+                    temp = FirstInput - SecondInput;
+                    PlotNumbers(temp);
+                    break;
+                case "+":
+                    temp = FirstInput + SecondInput;
+                    PlotNumbers(temp);
+                    break;
+                case "x":
+                    temp = FirstInput * SecondInput;
+                    PlotNumbers(temp);
+                    break;
+                case "/":
+                    temp = FirstInput / SecondInput;
+                    PlotNumbers(temp);
+                    break;
+            }
+    }
+    // plot text
+
+    void PlotNumbers(dynamic text)
+    {
+        string t = (string)text.ToString();
+        string fmt = t.Length > 9 ? "0.00000#E+0" : null; // high precision :)
+        sum.Text = (string)text.ToString(fmt);
+    }
+    void ValidateInput(object sender, TextChangedEventArgs e)
+    {
+        bool isValid = double.TryParse(e.NewTextValue, out double result) || String.IsNullOrEmpty(e.NewTextValue);
+        if (!isValid)
+        {
+            Entry entry = (Entry)sender;
+
+            //entry.Text = e.OldTextValue; //  bug crashes
+            entry.Text = entry.Text[..^1]; // remove last char
+            
+        }
+
+
+    }
+}
+
